ecf15fb8f570777e4623d585529a001b9bf05813
Edgard Perez Palma <eperezpalmav@uni.pe>
2018-07-15 00:55:13 -0500

    All Hosts

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = '4fq^(ef&!_xt5f0_(3=0z)%%fs)z3n4pmj=!0l!nt0xb%19h4e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '178.128.184.20', '.goblin.pe']
 
 # Application definition
 
