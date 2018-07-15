d2fd34f5002c715d039f778db1ab975cf5a6c658
Edgard Perez Palma <eperezpalmav@uni.pe>
2018-07-15 01:18:46 -0500

    Multihost

--- django_project/settings.py
+++ django_project/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE_CLASSES = (
-    'django.middleware.MultiHostMiddleware',
+    'goblin.multihost.MultiHostMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
