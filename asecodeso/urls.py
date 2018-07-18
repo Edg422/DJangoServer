05cec05b9ab6d25a149a2aee4ea3820c87817a0c
Edgard Perez Palma <eperezpalmav@uni.pe>
2018-07-17 19:27:17 -0500

    ASECODESO INDEX - STATIC FILES

--- asecodeso/urls.py
+++ asecodeso/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
+from django.conf.urls.static import static
 
 from . import views
+
+app_name = 'asecodeso'
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
