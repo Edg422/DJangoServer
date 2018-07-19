227f4cda71d9edf06a471072a80ca7c57531fea6
Edgard Perez Palma <eperezpalmav@uni.pe>
2018-07-19 17:13:39 -0500

    compress

--- asecodeso/urls.py
+++ asecodeso/urls.py
@@ -9,10 +9,4 @@ app_name = 'asecodeso'
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^favicon.ico$',
-        RedirectView.as_view(
-            url=staticfiles_storage.url('favicon.ico'),
-            permanent=False),
-        name="favicon"
-    ),
 ]
