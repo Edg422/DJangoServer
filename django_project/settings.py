3e98c895dfc4041526d1ccfcfdd49cef66374272
Edgard Perez Palma <eperezpalmav@uni.pe>
2018-07-20 14:16:53 -0500

    compress

--- django_project/settings.py
+++ django_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '4fq^(ef&!_xt5f0_(3=0z)%%fs)z3n4pmj=!0l!nt0xb%19h4e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '178.128.184.20', '.goblin.pe', '.asecodeso.com']
 
