02995d4e65276457888ab3f365a1119c375a81e5
Edgard Perez Palma <eperezpalmav@uni.pe>
2018-09-17 00:14:36 -0500

    prisma

--- prisma/views.py
+++ prisma/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Goblin Engineering and Consulting")
+    return HttpResponse("PRISMA Energy")
